1822d56b04a36cc072be792dd60c43a01cb280c2
TsukadaHaruto <146901202+TsukadaHaruto@users.noreply.github.com>
2024-09-27 02:09:40 +0900

    ?????????????????

--- main.cpp
+++ main.cpp
@@ -1,105 +1,34 @@
-#include "Audio.h"
-#include "AxisIndicator.h"
-#include "DirectXCommon.h"
-#include "GameScene.h"
-#include "ImGuiManager.h"
-#include "PrimitiveDrawer.h"
-#include "TextureManager.h"
-#include "WinApp.h"
+#include <iostream>
+#include <stdio.h>
+#include <thread>
+using namespace std;
 
-// Windowsアプリでのエントリーポイント(main関数)
-	// 汎用機能
-	Input* input = nullptr;
-	Audio* audio = nullptr;
-	AxisIndicator* axisIndicator = nullptr;
-	PrimitiveDrawer* primitiveDrawer = nullptr;
-	GameScene* gameScene = nullptr;
+void Quadruple(int num) {
+	num *= 4;
+	std::cout << "thread 1 =" << std::this_thread::get_id() << std::endl;
+}
 
-	// ゲームウィンドウの作成
-	win = WinApp::GetInstance();
-	win->CreateGameWindow();
+void AddFour(int num) {
+	num += 4;
+	std::cout << "thread 2 =" << std::this_thread::get_id() << std::endl;
+}
 
-	// DirectX初期化処理
-	dxCommon = DirectXCommon::GetInstance();
-	dxCommon->Initialize(win);
+void subtract(int num) {
+	num -= 4;
+	std::cout << "thread 3 =" << std::this_thread::get_id() << std::endl;
+}
 
-#pragma region 汎用機能初期化
-	// ImGuiの初期化
-	ImGuiManager* imguiManager = ImGuiManager::GetInstance();
-	imguiManager->Initialize(win, dxCommon);
+int main() {
+	int num = 3;
 
-	// 入力の初期化
-	input = Input::GetInstance();
-	input->Initialize();
+	std::thread th1(Quadruple, num);
+	th1.join();
 
-	// オーディオの初期化
-	audio = Audio::GetInstance();
-	audio->Initialize();
+	std::thread th2(AddFour, num);
+	th2.join();
 
-	// テクスチャマネージャの初期化
-	TextureManager::GetInstance()->Initialize(dxCommon->GetDevice());
-	TextureManager::Load("white1x1.png");
-
-	// スプライト静的初期化
-	Sprite::StaticInitialize(dxCommon->GetDevice(), WinApp::kWindowWidth, WinApp::kWindowHeight);
-
-	// 3Dモデル静的初期化
-	Model::StaticInitialize();
-
-	// 軸方向表示初期化
-	axisIndicator = AxisIndicator::GetInstance();
-	axisIndicator->Initialize();
-
-	primitiveDrawer = PrimitiveDrawer::GetInstance();
-	primitiveDrawer->Initialize();
-#pragma endregion
-
-	// ゲームシーンの初期化
-	gameScene = new GameScene();
-	gameScene->Initialize();
-
-	// メインループ
-	while (true) {
-		// メッセージ処理
-		if (win->ProcessMessage()) {
-			break;
-		}
-
-		// ImGui受付開始
-		imguiManager->Begin();
-		// 入力関連の毎フレーム処理
-		input->Update();
-		// ゲームシーンの毎フレーム処理
-		gameScene->Update();
-		// 軸表示の更新
-		axisIndicator->Update();
-		// ImGui受付終了
-		imguiManager->End();
-
-		// 描画開始
-		dxCommon->PreDraw();
-		// ゲームシーンの描画
-		gameScene->Draw();
-		// 軸表示の描画
-		axisIndicator->Draw();
-		// プリミティブ描画のリセット
-		primitiveDrawer->Reset();
-		// ImGui描画
-		imguiManager->Draw();
-		// 描画終了
-		dxCommon->PostDraw();
-	}
-
-	// 各種解放
-	delete gameScene;
-	// 3Dモデル解放
-	Model::StaticFinalize();
-	audio->Finalize();
-	// ImGui解放
-	imguiManager->Finalize();
-
-	// ゲームウィンドウの破棄
-	win->TerminateGameWindow();
+	std::thread th3(subtract, num);
+	th3.join();
 
 	return 0;
 }
